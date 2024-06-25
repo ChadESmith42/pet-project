8f78d2be922a28cb46317372637fab0138c35d5a
Chad Smith <chad.e.smith@outlook.com>
2024-06-24 19:03:40 -0500

    ADD route for pet to server

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const petRouter = require('./routers/pet.router');
 
 const app = express();
 
@@ -6,6 +7,8 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 const PORT = 3000;
+
+app.use('/api/pet', petRouter);
 
 app.listen(PORT, () => {
     console.info(`Server is listening on port ${PORT}.`);
