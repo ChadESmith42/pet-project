4b553f1ba74dbaddd828df0607eba3e95c23a8e0
Chad Smith <chad.e.smith@outlook.com>
2024-06-29 18:48:58 -0500

    ADD navigate back to list

--- client/src/components/pet/pet.component.ts
+++ client/src/components/pet/pet.component.ts
@@ -3,6 +3,7 @@ import { PetService } from '../../services/pet.service';
 import { Observable, of } from 'rxjs';
 import { Pet } from '../../models/pet';
 import { AsyncPipe, DatePipe } from '@angular/common';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-pet',
@@ -16,6 +17,7 @@ import { AsyncPipe, DatePipe } from '@angular/common';
 })
 export class PetComponent {
   private _petService: PetService = inject(PetService);
+  private _router: Router = inject(Router);
   pet$: Observable<Pet> = of({
     id: 0,
     name: '',
@@ -29,5 +31,9 @@ export class PetComponent {
   @Input()
   set id(petId: number) {
     this.pet$ = this._petService.getPet(petId);
+  }
+
+  goToList(): void {
+    this._router.navigate(['/pets']);
   }
 }
