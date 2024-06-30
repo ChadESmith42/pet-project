02e5ee60d26b418e937e88e04fe0ed2e9086ce6b
Chad Smith <chad.e.smith@outlook.com>
2024-06-29 20:44:09 -0500

    ADD environments for constants and configuration ADD content/styling for footer ADD footer component in app component

--- client/src/components/footer/footer.component.ts
+++ client/src/components/footer/footer.component.ts
@@ -1,12 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { environment } from '../../environments/environment.development';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-footer',
   standalone: true,
-  imports: [],
+  imports: [
+    MatButtonModule,
+    MatIconModule,
+    RouterLink,
+  ],
   templateUrl: './footer.component.html',
   styleUrl: './footer.component.scss'
 })
-export class FooterComponent {
+export class FooterComponent implements OnInit {
+  copyright: string = '';
+  startYear: number = environment.START_YEAR;
+
+  ngOnInit(): void {
+    this.copyright = this.setCopyright();
+  }
+
+  setCopyright(): string {
+    const now = new Date().getFullYear();
+    return now > this.startYear ? `${this.startYear}-${now}` : `${this.startYear}`
+  }
 
 }
