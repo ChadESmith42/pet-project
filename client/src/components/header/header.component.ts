b00219ba744e196dcda2f72f585207e90e0d3e55
Chad Smith <chad.e.smith@outlook.com>
2024-06-29 20:18:47 -0500

    ADD header logic and display in app component

--- client/src/components/header/header.component.ts
+++ client/src/components/header/header.component.ts
@@ -1,12 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatMenuModule } from '@angular/material/menu';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [],
+  imports: [
+    MatButtonModule,
+    MatIconModule,
+    MatMenuModule,
+    MatToolbarModule,
+  ],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss'
 })
 export class HeaderComponent {
+  private _router: Router = inject(Router);
 
+  goHome(): void {
+    this._router.navigate(['/']);
+  }
+
+  goToPets(): void {
+    this._router.navigate(['/pets']);
+  }
 }
